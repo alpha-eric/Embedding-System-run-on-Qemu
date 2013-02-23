82733e76c9b21541264ac38acab022c2e9e5a66d
psychesnet <psychesnet@gmail.com>
2013-02-23 15:11:18 +0800

    avoid append squashfs file.

--- ramfs/mkrootfs/dist/squashfs.mk
+++ ramfs/mkrootfs/dist/squashfs.mk
@@ -4,5 +4,5 @@ squashfs: mkbase \
        	mkbusybox \
        	mkhello
 	#cd $(ROOTFS) && find . -name *.so* | $(STRIP)
-	mksquashfs $(ROOTFS) $(PJ_PREFIX)/boot/$(ROOTFS_FILE)
+	mksquashfs $(ROOTFS) $(PJ_PREFIX)/boot/$(ROOTFS_FILE) -all-root -noappend
 	gzip -9 -c $(PJ_PREFIX)/boot/$(ROOTFS_FILE) > $(PJ_PREFIX)/boot/$(ROOTFS_FILE_WITH_GZ)
